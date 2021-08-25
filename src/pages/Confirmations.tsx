5456358040705e2b5888638e83418a995ac96548
kanir-l <kr@kanir.se>
2021-08-25 15:27:33 +0200

    routes, /confirmations, contact page, header and footer strucuturing pages

---
+++ src/pages/Confirmations.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Confirmations() {
+    return (
+        <React.Fragment>
+            <div className="confirmations-container">
+                <b>Thank you!</b><br></br>
+                <u>A confirmation has been sent to your email.</u><br></br>
+                <i>We’re looking forward to having you.</i>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Confirmations
