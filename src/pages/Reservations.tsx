1866e283b373e4fde9db2ebe17e5b4c26e10225a
Lovisa Bohman <lovisabo@hotmail.com>
2021-08-25 16:36:47 +0200

    Beginning to implement the react-calendar

--- src/pages/Reservations.tsx
+++ src/pages/Reservations.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
-import Button from '../components/Button'
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import Button from '../components/Button';
 
 function Reservations() {
+    const [date, onChange] = useState(new Date());
+
     return (
         <div className="reservations-container">
             <h2>Reservations</h2>
@@ -9,6 +13,8 @@ function Reservations() {
             <button onClick={() => {console.log("Minus clicked!")}}>Minus</button>
             <input type="number" name="numberOfGuests" id="numberOfGuests"></input>
             <button onClick={() => {console.log("Plus clicked!")}}>Plus</button>
+            <Calendar prev2Label={null} next2Label={null} minDetail="year" onChange={onChange} value={date}></Calendar>
+            {console.log(date)}
             <Button content="Continue" />
             <h4>Please select a time</h4>
             <Button content="18:00" />
