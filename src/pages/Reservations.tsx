a98dee428e4ad245ed4587e2a8019f4e3ceaf7cf
Lovisa Bohman <lovisabo@hotmail.com>
2021-08-25 11:37:59 +0200

    Hard-coded beginning on the reservation process.

--- src/pages/Reservations.tsx
+++ src/pages/Reservations.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Button from '../components/Button'
 
 function Reservations() {
-    
     return (
         <div className="reservations-container">
-            <p>Reservation Page works</p>
+            <h2>Reservations</h2>
+            <label htmlFor="numberOfGuests">Number of Guests</label>
+            <button onClick={() => {console.log("Minus clicked!")}}>Minus</button>
+            <input type="number" name="numberOfGuests" id="numberOfGuests"></input>
+            <button onClick={() => {console.log("Plus clicked!")}}>Plus</button>
+            <Button content="Continue"></Button>
         </div>
     )
 }
