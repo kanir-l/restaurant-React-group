1620212a9ec3edeac82bef35d707d75478f03cff
Lovisa Bohman <lovisabo@hotmail.com>
2021-08-26 14:10:03 +0200

    Move the two time slots into its own component,TimeSlots.tsx from Reservations.tsx file.

---
+++ src/pages/TimeSlots.tsx
@@ -0,0 +1,13 @@
+import Button from "../components/Button";
+
+function TimeSlots() {
+    return (
+        <div>
+            <h4>Available times</h4>
+            <Button content="18:00" />
+            <Button content="21:00" />
+        </div>
+    );
+}
+
+export default TimeSlots;
