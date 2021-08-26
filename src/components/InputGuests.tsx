18fca199064310b7bf4956952e892c6f2b337aaa
Lovisa Bohman <lovisabo@hotmail.com>
2021-08-26 13:48:08 +0200

    Created and moved code to InputGuests. Can change the value in the input field with buttons but not with keyboard.

--- src/components/InputGuests.tsx
+++ src/components/InputGuests.tsx
@@ -4,21 +4,16 @@ function InputGuests() {
     const [inputValue, setInputValue] = useState(0);
 
     const increment = () => {
-        setInputValue(inputValue + 1);
+        setInputValue(prevInputValue => prevInputValue + 1);
     }
 
-    // let reachedZero = false;
-    // if (inputValue >= 0) {
-    //         reachedZero = true;
-    //     }
-        
     const decrement = () => {
-        setInputValue(inputValue - 1);
+        setInputValue(prevInputValue => prevInputValue - 1);
     }
 
     return (
         <div>
-            <button /*{disabled={reachedZero}*/ onClick={decrement}>Decrement</button>
+            <button disabled={false} onClick={decrement}>Decrement</button>
             <input type="number" name="numberOfGuests" id="numberOfGuests" value={inputValue}></input>
             {/* <div>{inputValue}</div> */}
             <button onClick={increment}>Increment</button>
