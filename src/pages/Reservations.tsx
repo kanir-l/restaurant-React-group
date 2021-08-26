9175dc30babe52a98a65502c233af7c422b000d6
Lovisa Bohman <lovisabo@hotmail.com>
2021-08-26 10:44:32 +0200

    Move the number of guest part into its own component, InputGuests

--- src/pages/Reservations.tsx
+++ src/pages/Reservations.tsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import Button from '../components/Button';
+import InputGuests from '../components/InputGuests';
 
 function Reservations() {
-    const [date, onChange] = useState(new Date());
+    const [date, setDate] = useState(new Date());
 
     return (
         <div className="reservations-container">
@@ -14,12 +15,13 @@ function Reservations() {
             <input type="number" name="numberOfGuests" id="numberOfGuests"></input>
             <button onClick={() => {console.log("Plus clicked!")}}>Plus</button>
             <h4>Pick a Date</h4>
-            <Calendar prev2Label={null} next2Label={null} minDetail="year" onChange={onChange} value={date}></Calendar>
+            <Calendar prev2Label={null} next2Label={null} minDetail="year" onChange={setDate} value={date}></Calendar>
             {console.log(date)}
             <Button content="Continue" />
             <h4>Available times</h4>
             <Button content="18:00" />
             <Button content="21:00" />
+            <InputGuests></InputGuests>
         </div>
     )
 }
