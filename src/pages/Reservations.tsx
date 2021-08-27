643ad25daba66bd6daf97c2e35597beac8c724a2
Lovisa Bohman <lovisabo@hotmail.com>
2021-08-27 11:47:36 +0200

    Fix error, removed duplicate code and imports that wasn't in use

--- src/pages/Reservations.tsx
+++ src/pages/Reservations.tsx
@@ -1,6 +1,3 @@
-import React, { useState } from 'react';
-import { BookingModel } from '../models/BookingModel'
-import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import Button from '../components/Button';
 import ContactDetails from '../components/ContactDetails';
@@ -13,26 +10,11 @@ function Reservations() {
     return (
         <div className="reservations-container">
             <h2>Reservations</h2>
-
             <label htmlFor="numberOfGuests">Number of Guests</label>
             <InputGuests></InputGuests>
-
-
-            {/* <button onClick={() => {console.log("Minus clicked!")}}>Minus</button>
-            <input type="number" name="numberOfGuests" id="numberOfGuests"></input>
-            <button onClick={() => {console.log("Plus clicked!")}}>Plus</button> */}
-
-            <h4>Pick a Date</h4>
-            <Calendar prev2Label={null} next2Label={null} minDetail="year" onChange={setDate} value={date}></Calendar>
-            {console.log(date)}
-
-
             <InputDate></InputDate>
-
             <Button content="Continue" />
-
             <TimeSlots />
-
             <ContactDetails />
         </div>
     )
