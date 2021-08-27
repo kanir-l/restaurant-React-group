e9ee8bda10bd7c4480d2286f0d859638935264fc
Lovisa Bohman <lovisabo@hotmail.com>
2021-08-27 12:07:03 +0200

    Updated the input to use useState properly.

--- src/components/InputGuests.tsx
+++ src/components/InputGuests.tsx
@@ -11,10 +11,21 @@ function InputGuests() {
         setInputValue(prevInputValue => prevInputValue - 1);
     }
 
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(parseInt(e.target.value));
+    }
+
     return (
         <div>
             <button disabled={false} onClick={decrement}>Decrement</button>
-            <input type="number" name="numberOfGuests" id="numberOfGuests" value={inputValue}></input>
+            <input 
+                type="number"
+                value={inputValue}
+                onChange={handleChange}
+                name="numberOfGuests" 
+                id="numberOfGuests" 
+                />
+            {/* <input type="number" name="numberOfGuests" id="numberOfGuests" value={inputValue}></input> */}
             {/* <div>{inputValue}</div> */}
             <button onClick={increment}>Increment</button>
         </div>
