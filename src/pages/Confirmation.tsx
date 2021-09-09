20a5eb03adb190203866ac7606cf3bb2e59bd45d
LouLapins <loulapins@hotmail.com>
2021-09-09 18:16:04 +0200

    Edit  mediaqueries

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -3,9 +3,12 @@ import React from 'react'
 function Confirmation() {
     return (
         <div className="confirmation-container">
-            <h2>Thank you!</h2>
+            <img className="confirmation-img" src="../images/confirmation-photo.jpg" alt="Plate of dessert"></img>
+            <div className="confirmation">
+            <h3>Thank you!</h3>
             <p>A confirmation has been sent to your email.</p>
             <p>We’re looking forward to having you.</p>
+            </div>
         </div>
     )
 }
