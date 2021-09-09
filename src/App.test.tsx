853d3190b2d8d36bae7e4be87b660eebf40b0796
kanir-l <kr@kanir.se>
2021-09-09 16:24:49 +0200

    nav route testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,6 @@
  import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import userEvent from '@testing-library/user-event';
 import { Router } from 'react-router';
 import {createMemoryHistory} from 'history'
 
